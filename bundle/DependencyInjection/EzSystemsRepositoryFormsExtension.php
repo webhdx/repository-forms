df858a932da6162853f737082e629bbd0a9537c7
Maciej Kobus <maciej.kobus@ez.no>
2019-10-07 00:16:38 +0200

    wip

--- bundle/DependencyInjection/EzSystemsRepositoryFormsExtension.php
+++ bundle/DependencyInjection/EzSystemsRepositoryFormsExtension.php
@@ -8,23 +8,12 @@
  */
 namespace EzSystems\RepositoryFormsBundle\DependencyInjection;
 
-use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\Yaml\Yaml;
 
-class EzSystemsRepositoryFormsExtension extends Extension implements PrependExtensionInterface
+class EzSystemsRepositoryFormsExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-    }
-
-    public function prepend(ContainerBuilder $container)
-    {
-        $configFile = __DIR__ . '/../Resources/config/kernel.yaml';
-        $config = Yaml::parse(file_get_contents($configFile));
-        $container->prependExtensionConfig('ezpublish', $config);
-        $container->addResource(new FileResource($configFile));
     }
 }
