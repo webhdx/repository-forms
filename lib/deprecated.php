f7856baacd436031b3e37a5e019fbd18332c5484
Maciej Kobus <maciej.kobus@ez.no>
2019-11-19 14:31:33 +0100

    WIP

---
+++ lib/deprecated.php
@@ -0,0 +1,20 @@
+<?php
+\class_alias(
+    \EzSystems\EzPlatformAdminUi\FieldType\FieldDefinitionFormMapperInterface::class,
+    \EzSystems\RepositoryForms\FieldType\FieldDefinitionFormMapperInterface::class
+);
+
+\class_alias(
+    \EzSystems\EzPlatformContentForms\FieldType\FieldValueFormMapperInterface::class,
+    \EzSystems\RepositoryForms\FieldType\FieldValueFormMapperInterface::class
+);
+
+\class_alias(
+    \EzSystems\EzPlatformAdminUi\Form\Data\FieldDefinitionData::class,
+    \EzSystems\RepositoryForms\Data\FieldDefinitionData::class
+);
+
+\class_alias(
+    \EzSystems\EzPlatformContentForms\Data\Content\FieldData::class,
+    \EzSystems\RepositoryForms\Data\Content\FieldData::class
+);
